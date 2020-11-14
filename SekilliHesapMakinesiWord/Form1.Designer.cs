fc4656b354e23fdd2c967a05fed3f2703ed1a514
M. Utku Sarıoğlu <mustafautkusarioglu@gmail.com>
2020-11-14 20:01:33 +0300

    Add project files.

---
+++ SekilliHesapMakinesiWord/Form1.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace SekilliHesapMakinesiWord
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt0 = new System.Windows.Forms.Button();
+            this.bt2 = new System.Windows.Forms.Button();
+            this.bt00 = new System.Windows.Forms.Button();
+            this.bt5 = new System.Windows.Forms.Button();
+            this.bt4 = new System.Windows.Forms.Button();
+            this.bt3 = new System.Windows.Forms.Button();
+            this.bt8 = new System.Windows.Forms.Button();
+            this.bt7 = new System.Windows.Forms.Button();
+            this.bt6 = new System.Windows.Forms.Button();
+            this.btNokta = new System.Windows.Forms.Button();
+            this.bt9 = new System.Windows.Forms.Button();
+            this.bt1 = new System.Windows.Forms.Button();
+            this.btEksı = new System.Windows.Forms.Button();
+            this.btArtı = new System.Windows.Forms.Button();
+            this.btCarpma = new System.Windows.Forms.Button();
+            this.btBolme = new System.Windows.Forms.Button();
+            this.btEsıttır = new System.Windows.Forms.Button();
+            this.tbSonuc = new System.Windows.Forms.TextBox();
+            this.btButunbilgi = new System.Windows.Forms.Button();
+            this.lbSonuc = new System.Windows.Forms.Label();
+            this.btTextiSilme = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bt0
+            // 
+            this.bt0.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.bt0.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.bt0.Location = new System.Drawing.Point(26, 358);
+            this.bt0.Name = "bt0";
+            this.bt0.Size = new System.Drawing.Size(71, 51);
+            this.bt0.TabIndex = 0;
+            this.bt0.Text = "0";
+            this.bt0.UseVisualStyleBackColor = false;
+            this.bt0.Click += new System.EventHandler(this.RakamOlay);
+            // 
+            // bt2
+            // 
+            this.bt2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.bt2.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.bt2.Location = new System.Drawing.Point(26, 287);
+            this.bt2.Name = "bt2";
+            this.bt2.Size = new System.Drawing.Size(71, 51);
+            this.bt2.TabIndex = 1;
+            this.bt2.Text = "2";
+            this.bt2.UseVisualStyleBackColor = false;
+            this.bt2.Click += new System.EventHandler(this.RakamOlay);
+            // 
+            // bt00
+            // 
+            this.bt00.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.bt00.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.bt00.Location = new System.Drawing.Point(132, 358);
+            this.bt00.Name = "bt00";
+            this.bt00.Size = new System.Drawing.Size(71, 51);
+            this.bt00.TabIndex = 2;
+            this.bt00.Text = "00";
+            this.bt00.UseVisualStyleBackColor = false;
+            this.bt00.Click += new System.EventHandler(this.RakamOlay);
+            // 
+            // bt5
+            // 
+            this.bt5.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.bt5.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.bt5.Location = new System.Drawing.Point(26, 202);
+            this.bt5.Name = "bt5";
+            this.bt5.Size = new System.Drawing.Size(71, 51);
+            this.bt5.TabIndex = 3;
+            this.bt5.Text = "5";
+            this.bt5.UseVisualStyleBackColor = false;
+            this.bt5.Click += new System.EventHandler(this.RakamOlay);
+            // 
+            // bt4
+            // 
+            this.bt4.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.bt4.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.bt4.Location = new System.Drawing.Point(230, 287);
+            this.bt4.Name = "bt4";
+            this.bt4.Size = new System.Drawing.Size(71, 51);
+            this.bt4.TabIndex = 4;
+            this.bt4.Text = "4";
+            this.bt4.UseVisualStyleBackColor = false;
+            this.bt4.Click += new System.EventHandler(this.RakamOlay);
+            // 
+            // bt3
+            // 
+            this.bt3.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.bt3.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.bt3.Location = new System.Drawing.Point(132, 287);
+            this.bt3.Name = "bt3";
+            this.bt3.Size = new System.Drawing.Size(71, 51);
+            this.bt3.TabIndex = 5;
+            this.bt3.Text = "3";
+            this.bt3.UseVisualStyleBackColor = false;
+            this.bt3.Click += new System.EventHandler(this.RakamOlay);
+            // 
+            // bt8
+            // 
+            this.bt8.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.bt8.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.bt8.Location = new System.Drawing.Point(26, 131);
+            this.bt8.Name = "bt8";
+            this.bt8.Size = new System.Drawing.Size(71, 51);
+            this.bt8.TabIndex = 6;
+            this.bt8.Text = "8";
+            this.bt8.UseVisualStyleBackColor = false;
+            this.bt8.Click += new System.EventHandler(this.RakamOlay);
+            // 
+            // bt7
+            // 
+            this.bt7.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.bt7.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.bt7.Location = new System.Drawing.Point(230, 202);
+            this.bt7.Name = "bt7";
+            this.bt7.Size = new System.Drawing.Size(71, 51);
+            this.bt7.TabIndex = 7;
+            this.bt7.Text = "7";
+            this.bt7.UseVisualStyleBackColor = false;
+            this.bt7.Click += new System.EventHandler(this.RakamOlay);
+            // 
+            // bt6
+            // 
+            this.bt6.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.bt6.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.bt6.Location = new System.Drawing.Point(132, 202);
+            this.bt6.Name = "bt6";
+            this.bt6.Size = new System.Drawing.Size(71, 51);
+            this.bt6.TabIndex = 8;
+            this.bt6.Text = "6";
+            this.bt6.UseVisualStyleBackColor = false;
+            this.bt6.Click += new System.EventHandler(this.RakamOlay);
+            // 
+            // btNokta
+            // 
+            this.btNokta.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btNokta.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btNokta.Location = new System.Drawing.Point(230, 125);
+            this.btNokta.Name = "btNokta";
+            this.btNokta.Size = new System.Drawing.Size(71, 51);
+            this.btNokta.TabIndex = 9;
+            this.btNokta.Text = ".";
+            this.btNokta.UseVisualStyleBackColor = false;
+            this.btNokta.Click += new System.EventHandler(this.btNokta_Click);
+            // 
+            // bt9
+            // 
+            this.bt9.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.bt9.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.bt9.Location = new System.Drawing.Point(132, 131);
+            this.bt9.Name = "bt9";
+            this.bt9.Size = new System.Drawing.Size(71, 51);
+            this.bt9.TabIndex = 10;
+            this.bt9.Text = "9";
+            this.bt9.UseVisualStyleBackColor = false;
+            this.bt9.Click += new System.EventHandler(this.RakamOlay);
+            // 
+            // bt1
+            // 
+            this.bt1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.bt1.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.bt1.Location = new System.Drawing.Point(230, 358);
+            this.bt1.Name = "bt1";
+            this.bt1.Size = new System.Drawing.Size(71, 51);
+            this.bt1.TabIndex = 11;
+            this.bt1.Text = "1";
+            this.bt1.UseVisualStyleBackColor = false;
+            this.bt1.Click += new System.EventHandler(this.RakamOlay);
+            // 
+            // btEksı
+            // 
+            this.btEksı.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btEksı.Location = new System.Drawing.Point(342, 358);
+            this.btEksı.Name = "btEksı";
+            this.btEksı.Size = new System.Drawing.Size(59, 51);
+            this.btEksı.TabIndex = 12;
+            this.btEksı.Text = "-";
+            this.btEksı.UseVisualStyleBackColor = true;
+            this.btEksı.Click += new System.EventHandler(this.Hesapla);
+            // 
+            // btArtı
+            // 
+            this.btArtı.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btArtı.Location = new System.Drawing.Point(342, 131);
+            this.btArtı.Name = "btArtı";
+            this.btArtı.Size = new System.Drawing.Size(59, 51);
+            this.btArtı.TabIndex = 13;
+            this.btArtı.Text = "+";
+            this.btArtı.UseVisualStyleBackColor = true;
+            this.btArtı.Click += new System.EventHandler(this.Hesapla);
+            // 
+            // btCarpma
+            // 
+            this.btCarpma.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btCarpma.Location = new System.Drawing.Point(342, 202);
+            this.btCarpma.Name = "btCarpma";
+            this.btCarpma.Size = new System.Drawing.Size(59, 51);
+            this.btCarpma.TabIndex = 14;
+            this.btCarpma.Text = "x";
+            this.btCarpma.UseVisualStyleBackColor = true;
+            this.btCarpma.Click += new System.EventHandler(this.Hesapla);
+            // 
+            // btBolme
+            // 
+            this.btBolme.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btBolme.Location = new System.Drawing.Point(342, 281);
+            this.btBolme.Name = "btBolme";
+            this.btBolme.Size = new System.Drawing.Size(59, 51);
+            this.btBolme.TabIndex = 15;
+            this.btBolme.Text = "÷";
+            this.btBolme.UseVisualStyleBackColor = true;
+            this.btBolme.Click += new System.EventHandler(this.Hesapla);
+            // 
+            // btEsıttır
+            // 
+            this.btEsıttır.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btEsıttır.Location = new System.Drawing.Point(437, 281);
+            this.btEsıttır.Name = "btEsıttır";
+            this.btEsıttır.Size = new System.Drawing.Size(63, 128);
+            this.btEsıttır.TabIndex = 16;
+            this.btEsıttır.Text = "=";
+            this.btEsıttır.UseVisualStyleBackColor = true;
+            this.btEsıttır.Click += new System.EventHandler(this.btEsıttır_Click);
+            // 
+            // tbSonuc
+            // 
+            this.tbSonuc.Location = new System.Drawing.Point(26, 80);
+            this.tbSonuc.Name = "tbSonuc";
+            this.tbSonuc.Size = new System.Drawing.Size(254, 22);
+            this.tbSonuc.TabIndex = 17;
+            this.tbSonuc.Text = "0";
+            // 
+            // btButunbilgi
+            // 
+            this.btButunbilgi.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btButunbilgi.Location = new System.Drawing.Point(437, 204);
+            this.btButunbilgi.Name = "btButunbilgi";
+            this.btButunbilgi.Size = new System.Drawing.Size(63, 50);
+            this.btButunbilgi.TabIndex = 18;
+            this.btButunbilgi.Text = "C";
+            this.btButunbilgi.UseVisualStyleBackColor = true;
+            this.btButunbilgi.Click += new System.EventHandler(this.btButunbilgi_Click);
+            // 
+            // lbSonuc
+            // 
+            this.lbSonuc.AutoSize = true;
+            this.lbSonuc.Location = new System.Drawing.Point(23, 48);
+            this.lbSonuc.Name = "lbSonuc";
+            this.lbSonuc.Size = new System.Drawing.Size(0, 17);
+            this.lbSonuc.TabIndex = 19;
+            // 
+            // btTextiSilme
+            // 
+            this.btTextiSilme.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btTextiSilme.Location = new System.Drawing.Point(437, 131);
+            this.btTextiSilme.Name = "btTextiSilme";
+            this.btTextiSilme.Size = new System.Drawing.Size(63, 51);
+            this.btTextiSilme.TabIndex = 20;
+            this.btTextiSilme.Text = "CE";
+            this.btTextiSilme.UseVisualStyleBackColor = true;
+            this.btTextiSilme.Click += new System.EventHandler(this.btTextiSilme_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 434);
+            this.Controls.Add(this.btTextiSilme);
+            this.Controls.Add(this.lbSonuc);
+            this.Controls.Add(this.btButunbilgi);
+            this.Controls.Add(this.tbSonuc);
+            this.Controls.Add(this.btEsıttır);
+            this.Controls.Add(this.btBolme);
+            this.Controls.Add(this.btCarpma);
+            this.Controls.Add(this.btArtı);
+            this.Controls.Add(this.btEksı);
+            this.Controls.Add(this.bt1);
+            this.Controls.Add(this.bt9);
+            this.Controls.Add(this.btNokta);
+            this.Controls.Add(this.bt6);
+            this.Controls.Add(this.bt7);
+            this.Controls.Add(this.bt8);
+            this.Controls.Add(this.bt3);
+            this.Controls.Add(this.bt4);
+            this.Controls.Add(this.bt5);
+            this.Controls.Add(this.bt00);
+            this.Controls.Add(this.bt2);
+            this.Controls.Add(this.bt0);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt0;
+        private System.Windows.Forms.Button bt2;
+        private System.Windows.Forms.Button bt00;
+        private System.Windows.Forms.Button bt5;
+        private System.Windows.Forms.Button bt4;
+        private System.Windows.Forms.Button bt3;
+        private System.Windows.Forms.Button bt8;
+        private System.Windows.Forms.Button bt7;
+        private System.Windows.Forms.Button bt6;
+        private System.Windows.Forms.Button btNokta;
+        private System.Windows.Forms.Button bt9;
+        private System.Windows.Forms.Button bt1;
+        private System.Windows.Forms.Button btEksı;
+        private System.Windows.Forms.Button btArtı;
+        private System.Windows.Forms.Button btCarpma;
+        private System.Windows.Forms.Button btBolme;
+        private System.Windows.Forms.Button btEsıttır;
+        private System.Windows.Forms.TextBox tbSonuc;
+        private System.Windows.Forms.Button btButunbilgi;
+        private System.Windows.Forms.Label lbSonuc;
+        private System.Windows.Forms.Button btTextiSilme;
+    }
+}
+
---
+++ SekilliHesapMakinesiWord/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SekilliHesapMakinesiWord
+{
+    public partial class Form1 : Form
+    {
+        bool OptDurum;
+        double sonuc = 0;
+        string opt = "";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void RakamOlay(object sender, EventArgs e)
+        {
+            if (tbSonuc.Text == "0" || OptDurum)
+                tbSonuc.Clear();
+            OptDurum = false;
+
+            Button btn = (Button)sender;
+            tbSonuc.Text += btn.Text;
+        }
+
+        private void Hesapla(object sender, EventArgs e)
+        {
+            OptDurum = true;
+            Button btn = (Button) sender;
+            string YenıOpt = btn.Text;
+            lbSonuc.Text = lbSonuc.Text + " " + tbSonuc.Text + " " + YenıOpt;
+            switch (opt)
+            {
+                case "+": tbSonuc.Text = (sonuc + Double.Parse(tbSonuc.Text)).ToString(); 
+                     break;
+                case "-":
+                    tbSonuc.Text = (sonuc - Double.Parse(tbSonuc.Text)).ToString();
+                    break;
+                case "*":
+                    tbSonuc.Text = (sonuc * Double.Parse(tbSonuc.Text)).ToString();
+                    break;
+                case "/":
+                    tbSonuc.Text = (sonuc / Double.Parse(tbSonuc.Text)).ToString();
+                    break;
+            }
+            sonuc = double.Parse(tbSonuc.Text);
+            tbSonuc.Text = sonuc.ToString();
+            opt = YenıOpt;
+        }
+        private void btTextiSilme_Click(object sender, EventArgs e)
+        {
+            tbSonuc.Text = "0";
+        }
+
+        private void btButunbilgi_Click(object sender, EventArgs e)
+        {
+            tbSonuc.Text = "0";
+            lbSonuc.Text = "0";
+            opt = "";
+            sonuc = 0;
+            OptDurum = false;
+        }
+
+        private void btEsıttır_Click(object sender, EventArgs e)
+        {
+            lbSonuc.Text = "";
+            OptDurum = true;
+            switch (opt)
+            {
+                case "+":
+                    tbSonuc.Text = (sonuc + Double.Parse(tbSonuc.Text)).ToString();
+                    break;
+                case "-":
+                    tbSonuc.Text = (sonuc - Double.Parse(tbSonuc.Text)).ToString();
+                    break;
+                case "*":
+                    tbSonuc.Text = (sonuc * Double.Parse(tbSonuc.Text)).ToString();
+                    break;
+                case "/":
+                    tbSonuc.Text = (sonuc / Double.Parse(tbSonuc.Text)).ToString();
+                    break;
+            }
+            sonuc = double.Parse(tbSonuc.Text);
+            tbSonuc.Text = sonuc.ToString();
+            opt = "";
+
+        }
+
+        private void btNokta_Click(object sender, EventArgs e)
+        {
+            if (tbSonuc.Text == "0")
+            {
+                tbSonuc.Text = "0";
+            }
+            else if (OptDurum)  // tekrar sıfıra basmasını önlemek için
+            {
+                tbSonuc.Text = "0";
+            }
+            if(!tbSonuc.Text.Contains(","))
+            {
+                tbSonuc.Text += ","; // ondalık sayı eklesin
+            }
+            OptDurum = false;
+        }
+    }
+}
